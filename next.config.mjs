4e6e5ba7a20b60e6b21faad248b698574aa78742
ewof <marroq@marroq.xyz>
2025-03-17 21:10:14 -0400

    fix mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,13 @@
-import nextra from 'nextra';
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    if (config.cache && !dev) {
-      config.cache = Object.freeze({
-        type: 'memory',
-      });
-    }
-    // Important: return the modified config
-    return config;
-  },
-};
+import nextra from 'nextra'
 
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-});
-
-export default {
-  ...withNextra(nextConfig),
-  images: {
-    unoptimized: true,
+  latex: true,
+  search: {
+    codeblocks: false
   },
-};
+  contentDirBasePath: '/docs'
+})
+
+export default withNextra({
+  reactStrictMode: true
+})
